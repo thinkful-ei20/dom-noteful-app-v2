153c68d9b6e753b6d8732b5b8ec12dada68a8a07
mckoyd <dominick.mckoy@gmail.com>
2018-04-25 20:50:43 -0400

    Added routes for posting and updating folders

--- routes/folders.router.js
+++ routes/folders.router.js
@@ -23,11 +23,42 @@ router.get('/folders/:id', (req, res, next) => {
     .then(result => {
       if (result) {
         res.json(result[0]);
-      } else {
-        next();
-      }
+      } else { next();}
     })
     .catch(err => next(err));
+});
+
+// Update a folder
+router.put('/folders/:id', (req, res, next) => {
+  const qid = req.params.id;
+  const updateObj = {};
+  const updateField = 'name';
+  if (updateField in req.body) {
+    updateObj[updateField] = req.body[updateField];
+  }
+  if (!updateObj.name) {
+    const err = new Error('Missing the `name` in request body bruh.');
+    err.status = 400; return next(err);
+  }
+  knex('folders')
+    .update(updateObj)
+    .where({id: qid})
+    .returning(['id', 'name'])
+    .then(folder => res.json(folder[0]))
+    .catch(err => {next(err);});
+});
+
+router.post('/folders', (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    const err = new Error('Missing the `name` in request body bruh.');
+    err.status = 400; return next(err);
+  }
+  knex('folders')
+    .insert({ name })
+    .returning(['id', 'name'])
+    .then(folder => res.location(`http://${req.headers.host}/api/folders/${folder[0].id}`).status(201).json(folder))
+    .catch(err => { next(err); });
 });
 
 module.exports = router;
